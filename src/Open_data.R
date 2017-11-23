d45ea4021d9e2789b8677c62488dec3579435a4f
Amy Goldlist <amygoldlist@gmail.com>
2017-11-23 15:44:42 -0800

    added documentation to script

--- src/Open_data.R
+++ src/Open_data.R
@@ -1,7 +1,17 @@
-###Open_data.Rscript
-###This Script opens the data file and looks at things
+#! /usr/bin/env Rscript 
+# Open_data.Rscript
+# Amy Goldlist, Nov 2017
+#
+# This Script opens the data file and looks at things
+# Usage:  run and it will open the data file.
 
 
+
+#reads in data
 baby_data <- read.csv("data/01023509_eng.csv")
 
+#sumarizes the data
 summary(baby_data)
+
+
+
