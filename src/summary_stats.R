cd5d12ee16ad99fa81debced71c595c667cc2542
Amy Goldlist <amygoldlist@gmail.com>
2017-12-09 09:12:30 -0800

    changes to make command line inputs

--- src/summary_stats.R
+++ src/summary_stats.R
@@ -1,10 +1,31 @@
-###load tidyvers
+
+##summary_stats.R
+##by Amy Goldlist, Decmenber 2017
+
+## Usage:  Rscript src/summary_stats.R $data $new_filename
+## the original filename is "results/baby_data.csv" 
+## the target filename is "results/summary_stats.csv"
+
+
+
+
+## This script takes the clean data, and finds the summary stats needed to run a t-test 
+
+
+# read in command line argument
+args <- commandArgs(trailingOnly = TRUE)
+input_file <- args[1]
+output_file <- args[2]
+
+
+
+###load libraries needed
 library(tidyverse)
 library(SDMTools)
 
 ##load data
-baby_data <- read.csv("results/baby_data.csv")
-
+##baby_data <- read.csv("results/baby_data.csv")
+baby_data <- read.csv(input_file)
 
 baby_data %>% 
   arrange(Value) %>% 
@@ -18,5 +39,5 @@ summary_stats <- baby_data %>%
             standard_deviation = sqrt(variance),
             count = sum(Value))
 
-write.csv(summary_stats, "results/summary_stats.csv")
-
+##write.csv(summary_stats, "results/summary_stats.csv")
+write.csv(summary_stats, output_file)
